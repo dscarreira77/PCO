f33b176d7f97d1dc6ec15d68db524b205b4b8ef7
Tomás Alves <102360450+Tashmacher@users.noreply.github.com>
2023-11-20 16:57:51 +0000

    Cup + Table

--- PCO_projeto/src/types/Cup.java
+++ PCO_projeto/src/types/Cup.java
@@ -1,25 +1,31 @@
 package types;
 
-//Notem que podem faltar métodos na classe que permitam lidar melhor com os objectos.
-public class Cup extends Bottle{
-	//TODO
+public class Cup extends Bottle {
+    public static final int CUP_SIZE = 1;
+    private static final int TIMES_OF_USAGES = 3;
+    private int usages;
 
-	public static final int CUP_SIZE = 1; 
+    public Cup() {
+        super(CUP_SIZE);
+        this.usages = 0; 
+    }
 
-	private static final int TIMES_OF_USAGES = 3;
+    public Cup(Filling[] content) {
+        super(content.length > 0 ? content[0] : null); 
+        this.usages = 0;
+    }
 
-	public static String empty = "⚪";
-
-
-	public Cup() {
-	}
-
-
-	public Cup(int size) {
-	}
-
-	public Cup(Filling[] bottle) {
-	}
-
-
+    @Override
+    public boolean receive(Filling s) {
+        if (usages >= TIMES_OF_USAGES) {
+            System.out.println("Cup has reached maximum usages and cannot receive more content.");
+            return false; 
+        } else {
+            boolean received = super.receive(s);
+            if (received) {
+                usages++;
+            }
+            return received;
+        }
+    }
 }
--- PCO_projeto/src/types/Table.java
+++ PCO_projeto/src/types/Table.java
@@ -1,109 +1,113 @@
 package types;
 
-//Notem que podem faltar métodos na classe que permitam lidar melhor com os objectos.
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Table {
 
-	public static String EOL = System.lineSeparator();
+    public static String EOL = System.lineSeparator();
+    public static final String empty = "⬜";
+    public static final int DIFFICULTY = 3;
+    public static final int DEFAULT_BOTTLE_SIZE = 5;
 
-	public static final String empty = "⬜";
+    private List<Bottle> bottles;
 
-	public static final int DIFICULTY = 3;
+    public Table(Filling[] symbols, int numberOfUsedSymbols, int seed, int bottleSize) {
+        int minUsedSymbols = Math.min(numberOfUsedSymbols, symbols.length + DIFFICULTY);
+        this.bottles = new ArrayList<>();
+        Random random = new Random(seed);
 
-	public static final int DEFAULT_BOTTLE_SIZE = 5; 
+        for (int i = 0; i < symbols.length; i++) {
+            Filling[] bottleContent = new Filling[bottleSize];
+            for (int j = 0; j < bottleSize; j++) {
+                bottleContent[j] = symbols[random.nextInt(minUsedSymbols)];
+            }
+            Bottle bottle = new Bottle(bottleContent);
+            bottles.add(bottle);
+        }
+    }
+
+    public void regenerateTable() {
+        Random random = new Random();
+        for (Bottle bottle : bottles) {
+            Filling[] newContent = new Filling[bottle.size()];
+            for (int j = 0; j < bottle.size(); j++) {
+                newContent[j] = bottle.getContent()[random.nextInt(bottle.size())];
+            }
+            bottle.setContent(newContent);
+        }
+    }
+
+    public boolean singleFilling(int x) {
+        if (x >= 0 && x < bottles.size()) {
+            return bottles.get(x).isSingleFilling();
+        }
+        return false;
+    }
+
+    public boolean isEmpty(int x) {
+        if (x >= 0 && x < bottles.size()) {
+            return bottles.get(x).isEmpty();
+        }
+        return false;
+    }
+
+    public boolean isFull(int x) {
+        if (x >= 0 && x < bottles.size()) {
+            return bottles.get(x).isFull();
+        }
+        return false;
+    }
+
+    public boolean areAllFilled() {
+        for (Bottle bottle : bottles) {
+            if (!bottle.isEmpty() && !bottle.isSingleFilling()) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 
-	/**
-	 * 
-	 * @param symbols
-	 * @param numberOfUsedSymbols
-	 * @param seed
-	 * @param bootleSize
-	 */
-	public Table(Filling[] symbols, int numberOfUsedSymbols, int seed, int bootleSize) {
-	}
-
-	/**
-	 * 
-	 */
-	public void regenerateTable() {
-	}
-
-	/**
-	 * 
-	 * @param x
-	 * @return
-	 */
-	public boolean singleFilling(int x) {
-		return false;
-	}
-
-	/**
-	 * 
-	 * @param x
-	 * @return
-	 */
-	public boolean isEmpty(int x) {
-		return false;
-	}
-
-	/**
-	 * 
-	 * @param x
-	 * @return
-	 */
-	public boolean isFull(int x) {
-		return false;
-	}
-
-	/**
-	 * 
-	 * @return
-	 */
-	public boolean areAllFilled() {
-		return false;
-	}
-
-
-	/**
-	 * 
-	 * @param i
-	 * @param j
-	 */
 	public void pourFromTo(int i, int j) {
-		
-	}
+        if (i >= 0 && i < bottles.size() && j >= 0 && j < bottles.size() && i != j) {
+            Bottle sourceBottle = bottles.get(i);
+            Bottle targetBottle = bottles.get(j);
+            
+            if (!sourceBottle.isEmpty() && !targetBottle.isFull()) {
+                Filling topFilling = sourceBottle.top();
+                boolean poured = targetBottle.receive(topFilling);
+                if (poured) {
+                    sourceBottle.pourOut();
+                }
+            }
+        }
+    }
 
-	/**
-	 * 
-	 * @param bottle
-	 */
-	public void addBootle(Bottle bottle) {
-		
-	}
+    public void addBootle(Bottle bottle) {
+        if (bottle != null) {
+            bottles.add(bottle);
+        }
+    }
 
-	/**
-	 * 
-	 * @return
-	 */
-	public int getSizeBottles() {
-		return 0;
-	}
+    public int getSizeBottles() {
+        return bottles.size();
+    }
 
+    public Filling top(int x) {
+        if (x >= 0 && x < bottles.size() && !bottles.get(x).isEmpty()) {
+            return bottles.get(x).top();
+        }
+        return null;
+    }
 
-	/**
-	 * 
-	 * @param x
-	 * @return
-	 */
-	public Filling top(int x) {
-		return null;
-	}
-
-	/**
-	 * 
-	 */
-	public String toString() {
-		return null;
-	}
-
+    @Override
+    public String toString() {
+        StringBuilder tableContent = new StringBuilder();
+        for (int i = 0; i < bottles.size(); i++) {
+            tableContent.append("Bottle ").append(i).append(": ").append(bottles.get(i).toString()).append(EOL);
+        }
+        return tableContent.toString();
+    }
 }
