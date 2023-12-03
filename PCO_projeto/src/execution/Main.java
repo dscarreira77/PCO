44c9002435f9f48cb8ce015246f7f68ab20812e7
Tomás Alves <102360450+Tashmacher@users.noreply.github.com>
2023-12-03 23:41:10 +0000

    Main done

--- PCO_projeto/src/execution/Main.java
+++ PCO_projeto/src/execution/Main.java
@@ -1,10 +1,16 @@
 package execution;
 import java.util.Scanner;
-import types.Table;
+
+import java.util.Random;
+import types.Filling;
+import types.FinalScoringFillingGame;
+import types.Animals;
+import types.BettingFillingGame;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
 
         System.out.println("Type 'play' to start the game:");
         String input = scanner.nextLine();
@@ -18,36 +24,58 @@ public class Main {
 			int theme = scanner.nextInt();
 			if (theme == 1) {
 				System.out.println("You chose animals.");
-				System.out.println("Choose THe number of bottles.");
-				System.out.println("From 3 to how many you would like to play with.");
-				int dificulty = scanner.nextInt();
 				System.out.println("Choose a mode.");
 				System.out.println("1 - Final Score Mode");
 				System.out.println("2 - Betting Mode");
 				int mode = scanner.nextInt();
 				if (mode == 1) {
-					System.out.println("Choose a final score.");
-					int finalScore = scanner.nextInt();
-					System.out.println("Choose a bet.");
-					int bet = scanner.nextInt();
-					System.out.println("Choose a number of plays.");
-					int maxPlays = scanner.nextInt();
-					Table table = new Table(dificulty);
-					while (table.nrMoves < maxPlays) {
-						table.addBootle(table.getNewBottle());
+					FinalScoringFillingGame game;
+					Filling[] symbols = Animals.values();
+					int numberOfUsedSymbols = 3;
+					System.out.println("Choose a seed.");
+					int seedUser = scanner.nextInt();
+					int seed = random.nextInt(seedUser)+1;
+					System.out.println("Choose a bottle size. 5 will be used by default.");
+					int bootleSize = scanner.nextInt();
+					if(bootleSize == 0) {
+						bootleSize = 5;
 					}
-					System.out.println(table.toString());
+					game = new FinalScoringFillingGame(symbols, numberOfUsedSymbols, seed, bootleSize);
+					while(!game.isRoundFinished()) {
+						System.out.println("Insert the number of the bottle you want to pour from.");
+						int bottlePourNumber = scanner.nextInt();
+						System.out.println("Insert the number of the bottle you want to fill.");
+						int bottleFillNumber = scanner.nextInt();
+						game.play(bottlePourNumber, bottleFillNumber);
+						System.out.println(game.toString());
+					}
 				}
 				else if (mode == 2) {
+					BettingFillingGame game;
+					Filling[] symbols = Animals.values();
+					int numberOfUsedSymbols = 3;
+					System.out.println("Choose a seed.");
+					int seedUser = scanner.nextInt();
+					int seed = random.nextInt(seedUser)+1;
+					System.out.println("Choose a bottle size. 5 will be used by default.");
+					int bootleSize = scanner.nextInt();
+					if(bootleSize == 0) {
+						bootleSize = 5;
+					}
+					int score = 0;
 					System.out.println("Choose a bet.");
 					int bet = scanner.nextInt();
-					System.out.println("Choose a number of plays.");
+					System.out.println("Choose the max number of plays.");
 					int maxPlays = scanner.nextInt();
-					Table table = new Table(dificulty);
-					while (table.nrMoves < maxPlays) {
-						table.addBootle(table.getNewBottle());
+					game = new BettingFillingGame(symbols, numberOfUsedSymbols, seed, bootleSize, score, bet, maxPlays);
+					while(!game.isRoundFinished()) {
+						System.out.println("Insert the number of the bottle you want to pour from.");
+						int bottlePourNumber = scanner.nextInt();
+						System.out.println("Insert the number of the bottle you want to fill.");
+						int bottleFillNumber = scanner.nextInt();
+						game.play(bottlePourNumber, bottleFillNumber);
+						System.out.println(game.toString());
 					}
-					System.out.println(table.toString());
 				}
 				else {
 					System.out.println("Invalid mode. Please choose a valid mode.");
@@ -55,36 +83,58 @@ public class Main {
 			}
 			else if (theme == 2) {
 				System.out.println("You chose emojis.");
-				System.out.println("Choose THe number of bottles.");
-				System.out.println("From 3 to how many you would like to play with.");
-				int dificulty = scanner.nextInt();
 				System.out.println("Choose a mode.");
 				System.out.println("1 - Final Score Mode");
 				System.out.println("2 - Betting Mode");
 				int mode = scanner.nextInt();
 				if (mode == 1) {
-					System.out.println("Choose a final score.");
-					int finalScore = scanner.nextInt();
-					System.out.println("Choose a bet.");
-					int bet = scanner.nextInt();
-					System.out.println("Choose a number of plays.");
-					int maxPlays = scanner.nextInt();
-					Table table = new Table(dificulty);
-					while (table.nrMoves < maxPlays) {
-						table.addBootle(table.getNewBottle());
+					FinalScoringFillingGame game;
+					Filling[] symbols = Animals.values();
+					int numberOfUsedSymbols = 3;
+					System.out.println("Choose a seed.");
+					int seedUser = scanner.nextInt();
+					int seed = random.nextInt(seedUser)+1;
+					System.out.println("Choose a bottle size. 5 will be used by default.");
+					int bootleSize = scanner.nextInt();
+					if(bootleSize == 0) {
+						bootleSize = 5;
 					}
-					System.out.println(table.toString());
+					game = new FinalScoringFillingGame(symbols, numberOfUsedSymbols, seed, bootleSize);
+					while(!game.isRoundFinished()) {
+						System.out.println("Insert the number of the bottle you want to pour from.");
+						int bottlePourNumber = scanner.nextInt();
+						System.out.println("Insert the number of the bottle you want to fill.");
+						int bottleFillNumber = scanner.nextInt();
+						game.play(bottlePourNumber, bottleFillNumber);
+						System.out.println(game.toString());
+					}
 				}
 				else if (mode == 2) {
+					BettingFillingGame game;
+					Filling[] symbols = Animals.values();
+					int numberOfUsedSymbols = 3;
+					System.out.println("Choose a seed.");
+					int seedUser = scanner.nextInt();
+					int seed = random.nextInt(seedUser)+1;
+					System.out.println("Choose a bottle size. 5 will be used by default.");
+					int bootleSize = scanner.nextInt();
+					if(bootleSize == 0) {
+						bootleSize = 5;
+					}
+					int score = 0;
 					System.out.println("Choose a bet.");
 					int bet = scanner.nextInt();
-					System.out.println("Choose a number of plays.");
+					System.out.println("Choose the max number of plays.");
 					int maxPlays = scanner.nextInt();
-					Table table = new Table(dificulty);
-					while (table.nrMoves < maxPlays) {
-						table.addBootle(table.getNewBottle());
+					game = new BettingFillingGame(symbols, numberOfUsedSymbols, seed, bootleSize, score, bet, maxPlays);
+					while(!game.isRoundFinished()) {
+						System.out.println("Insert the number of the bottle you want to pour from.");
+						int bottlePourNumber = scanner.nextInt();
+						System.out.println("Insert the number of the bottle you want to fill.");
+						int bottleFillNumber = scanner.nextInt();
+						game.play(bottlePourNumber, bottleFillNumber);
+						System.out.println(game.toString());
 					}
-					System.out.println(table.toString());
 				}
 				else {
 					System.out.println("Invalid mode. Please choose a valid mode.");
@@ -92,36 +142,58 @@ public class Main {
 			}
 			else if (theme == 3) {
 				System.out.println("You chose balls.");
-				System.out.println("Choose THe number of bottles.");
-				System.out.println("From 3 to how many you would like to play with.");
-				int dificulty = scanner.nextInt();
 				System.out.println("Choose a mode.");
 				System.out.println("1 - Final Score Mode");
 				System.out.println("2 - Betting Mode");
 				int mode = scanner.nextInt();
 				if (mode == 1) {
-					System.out.println("Choose a final score.");
-					int finalScore = scanner.nextInt();
-					System.out.println("Choose a bet.");
-					int bet = scanner.nextInt();
-					System.out.println("Choose a number of plays.");
-					int maxPlays = scanner.nextInt();
-					Table table = new Table(dificulty);
-					while (table.nrMoves < maxPlays) {
-						table.addBootle(table.getNewBottle());
+					FinalScoringFillingGame game;
+					Filling[] symbols = Animals.values();
+					int numberOfUsedSymbols = 3;
+					System.out.println("Choose a seed.");
+					int seedUser = scanner.nextInt();
+					int seed = random.nextInt(seedUser)+1;
+					System.out.println("Choose a bottle size. 5 will be used by default.");
+					int bootleSize = scanner.nextInt();
+					if(bootleSize == 0) {
+						bootleSize = 5;
 					}
-					System.out.println(table.toString());
+					game = new FinalScoringFillingGame(symbols, numberOfUsedSymbols, seed, bootleSize);
+					while(!game.isRoundFinished()) {
+						System.out.println("Insert the number of the bottle you want to pour from.");
+						int bottlePourNumber = scanner.nextInt();
+						System.out.println("Insert the number of the bottle you want to fill.");
+						int bottleFillNumber = scanner.nextInt();
+						game.play(bottlePourNumber, bottleFillNumber);
+						System.out.println(game.toString());
+					}
 				}
 				else if (mode == 2) {
+					BettingFillingGame game;
+					Filling[] symbols = Animals.values();
+					int numberOfUsedSymbols = 3;
+					System.out.println("Choose a seed.");
+					int seedUser = scanner.nextInt();
+					int seed = random.nextInt(seedUser)+1;
+					System.out.println("Choose a bottle size. 5 will be used by default.");
+					int bootleSize = scanner.nextInt();
+					if(bootleSize == 0) {
+						bootleSize = 5;
+					}
+					int score = 0;
 					System.out.println("Choose a bet.");
 					int bet = scanner.nextInt();
-					System.out.println("Choose a number of plays.");
+					System.out.println("Choose the max number of plays.");
 					int maxPlays = scanner.nextInt();
-					Table table = new Table(dificulty);
-					while (table.nrMoves < maxPlays) {
-						table.addBootle(table.getNewBottle());
+					game = new BettingFillingGame(symbols, numberOfUsedSymbols, seed, bootleSize, score, bet, maxPlays);
+					while(!game.isRoundFinished()) {
+						System.out.println("Insert the number of the bottle you want to pour from.");
+						int bottlePourNumber = scanner.nextInt();
+						System.out.println("Insert the number of the bottle you want to fill.");
+						int bottleFillNumber = scanner.nextInt();
+						game.play(bottlePourNumber, bottleFillNumber);
+						System.out.println(game.toString());
 					}
-					System.out.println(table.toString());
 				}
 				else {
 					System.out.println("Invalid mode. Please choose a valid mode.");
